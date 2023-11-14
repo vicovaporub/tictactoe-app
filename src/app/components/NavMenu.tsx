b3a8ec85973b9400a36d111079ecddb25a149fc1
Victor Castro <victor.castro34@gmail.com>
2023-11-14 19:31:33 -0300

    first commit

---
+++ src/app/components/NavMenu.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { signIn, signOut, useSession } from "next-auth/react"
+
+const AuthButton = () => {
+    const { data: session } = useSession()
+
+    if (session) {
+        return (
+            <>
+            {session?.user?.name} <br/>
+            <button onClick={() => signOut()}>Sign Out</button>
+            </>
+        )
+    }
+
+    return (
+        <>
+        Not signed in <br/>
+        <button onClick={() => signIn()}>Sign in</button>
+        </>
+    )
+}
+
+
+export const NavMenu = () => {
+    return (
+        <AuthButton/>
+    )
+}
