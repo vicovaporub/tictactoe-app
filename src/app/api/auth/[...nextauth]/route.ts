e4dcf756bd0b3d8269114e2e064249b962503e57
Victor Castro <victor.castro34@gmail.com>
2023-11-16 10:27:48 -0300

    fix: added auth.ts to fix error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,5 @@
-import NextAuth from "next-auth/next";
-import GitHubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
-
-const authOptions = {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID ?? "",
-            clientSecret: process.env.GITHUB_SECRET ?? "",
-        }),
-        GoogleProvider ({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-          })
-    ]
-}
+import NextAuth from "next-auth/next"
+import { authOptions } from "./auth"
 
 export const handler = NextAuth(authOptions)
 
