c44a3d205ae4edc113a589d01b08566e9d2b15bc
Victor Castro <victor.castro34@gmail.com>
2023-11-23 15:35:35 -0300

    fix: fixed typo in Game component

--- src/app/components/Game/Game.tsx
+++ src/app/components/Game/Game.tsx
@@ -8,7 +8,7 @@ const Game = async () => {
     if (!session || !session.user) {
         return (
             <div className="home-container">
-            <div className="welcome-title">Welcome to Vico's Wondrous Tic Tac Toe Game</div>
+            <div className="welcome-title">Welcome to my Wondrous Tic Tac Toe Game</div>
             <div className="login-alert">Please, log-in so you can play!</div>
             </div>
 
