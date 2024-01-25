d9bdf5da27a3396d7cf79116dfb7632c1725a84c
Victor Castro <victor.castro34@gmail.com>
2024-01-25 18:10:22 -0300

    fix: fixed to run vercel

--- src/app/components/GamePageSocket/GamePageSocket.tsx
+++ src/app/components/GamePageSocket/GamePageSocket.tsx
@@ -5,31 +5,31 @@ import { io } from 'socket.io-client';
 
 export default function GamePageSocket() {
 
-    // Create a socket connection when the component mounts
- const socket = io('http://localhost:5000');
+/* TODO: SOCKET
 
- // Set up event listeners when the component mounts
+const socket = io('http://localhost:5000');
+
  useEffect(() => {
-   // Log socket ID on connection
+   
    const handleConnect = () => {
      console.log('Connected with ID:', socket.id);
    };
 
-   // Log socket ID on disconnection
+
    const handleDisconnect = () => {
      console.log('Disconnected with ID:', socket.id);
    };
 
-   // Set up event listeners
+ 
    socket.on('connect', handleConnect);
    socket.on('disconnect', handleDisconnect);
 
-   // Clean up the event listeners when the component is unmounted
+   
    return () => {
      socket.off('connect', handleConnect);
      socket.off('disconnect', handleDisconnect);
    };
- }, [socket]); // Include socket in the dependency array to avoid ESLint warnings
+ }, [socket]); */
 
   return (
     <div className="game-page">
